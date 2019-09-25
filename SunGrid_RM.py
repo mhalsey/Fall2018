5e11f5857f5609b9bc460b01fbcb9225e876ce76
mhalsey <36709563+mhalsey@users.noreply.github.com>
2019-09-25 12:27:05 -0500

    Update SunGrid_RM.py

--- SunGrid_RM.py
+++ SunGrid_RM.py
@@ -22,7 +22,7 @@ print os.getcwd()
 
 #os.chdir(('c:\\Users\\where\\work\\will\be\done'))
 
-#def dir_path(w):
+#def dir_path():
 #    if os.path.isdir():
 #        return working_directory
 #    else:
@@ -32,21 +32,31 @@ print os.getcwd()
 #Unless we like typing out paths
 
 #Begin parsing arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("-p", "--path", type=dir_path)
-parser.add_argument("-f", "--twoBit_file", type=file, choices=['.2bit'], required=True, help="Where genome file is in .2bit")
-parser.add_argument("-b", "--batch_count", type=int, default=10, help="How many batches are being processed")
-parser.add_argument("-s","--species", required=True, help="Species name")
-parser.add_argument("-l", "--library", type=file, choices=['.fa,.fas'], help="Transposable element library in .fasta file")
-parser.add_argument("-e", "engine", help="Choo! Choo! Kidding. Search engine used, ex. UCSC")
-parser.add_argument("-g" "--genomeDir", type=dir_path, required=True, help="Directory where genome is found")
-#parser.add_argument("-n", "--nolow", help="I have no idea what this does")
-#parser.add_argument("-i", "--inv", help="Not sure what this does either")
+def get_args()"
 
-args = parser.parse_args()
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-p", "--path", type=dir_path)
+	parser.add_argument("-f", "--twoBit_file", type=file, choices=['.2bit'], required=True, help="Where genome file is in .2bit")
+	parser.add_argument("-b", "--batch_count", type=int, default=10, help="How many batches are being processed")
+	parser.add_argument("-s","--species", required=True, help="Species name")
+	parser.add_argument("-l", "--library", type=file, choices=['.fa,.fas'], help="Transposable element library in .fasta file")
+	parser.add_argument("-e", "engine", help="Choo! Choo! Kidding. Search engine used, ex. UCSC")
+	parser.add_argument("-g" "--genomeDir", type=dir_path, required=True, help="Directory where genome is found")
+	#parser.add_argument("-n", "--nolow", help="I have no idea what this does")
+	#parser.add_argument("-i", "--inv", help="Not sure what this does either")
 
-GENOME = args.twoBit_file
-COUNT = args.batch_count
+	args = parser.parse_args()
+
+	GENOME = args.twoBit_file
+	COUNT = args.batch_count
+	SPECIES = arg.species
+	LIBRARY = args.library
+	ENGINE = args.engine
+	G_DIRECTORY = args.genomeDir
+
+GENOME, COUNT, SPECIES, LIBRARY, ENGINE, G_DIRECTORY = get_args()
+
+	return = get_args()
 		
 if args.species:
     print("Processing...")
